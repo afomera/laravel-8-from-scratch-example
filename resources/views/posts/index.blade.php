2847f773031391d07c7825b33d6a79bc2e62e972
Andrea Fomera <afomera@hey.com>
2022-03-12 12:24:53 -0700

    Add pagination support

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -4,6 +4,10 @@
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
         @if ($posts->count())
             <x-posts-grid :posts="$posts" />
+
+            <div class="mt-6">
+                {{ $posts->links() }}
+            </div>
         @else
             <p class="text-center">No posts yet, check back later</p>
         @endif
