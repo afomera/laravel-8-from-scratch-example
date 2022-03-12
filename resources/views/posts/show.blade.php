056becc30b567340c6e9d19eadb5bc4528baf0e9
Andrea Fomera <afomera@hey.com>
2022-03-12 16:07:45 -0700

    Write the markup for the post comment component

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -48,23 +48,15 @@
                         {!! $post->body !!}
                     </div>
                 </div>
+
+                <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    <x-post-comment />
+                    <x-post-comment />
+                    <x-post-comment />
+                    <x-post-comment />
+
+                </section>
             </article>
         </main>
     </section>
 </x-layout>
-<!-- 
-
-    <article>
-      <h1>{{ $post->title }}</h1>
-      <p>
-        By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}">
-          {{ $post->category->name }}
-        </a>
-      </p>
-
-      <div>
-        {!! $post->body !!}
-      </div>
-    </article>
-
-    <a href="/">Go back</a> -->
