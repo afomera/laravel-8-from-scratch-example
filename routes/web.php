1042159501b4897ad959dcb026e09b2d926fc01b
Andrea Fomera <afomera@hey.com>
2022-03-12 09:50:11 -0700

    named route and dropdown icon component

--- routes/web.php
+++ routes/web.php
@@ -25,7 +25,7 @@ Route::get('/', function () {
         "posts" => Post::latest()->get(),
         "categories" => Category::all(),
     ]);
-});
+})->name("home");
 
 Route::get("posts/{post:slug}", function(Post $post){
     return view("post", ["post" => $post]);
@@ -37,7 +37,7 @@ Route::get("categories/{category:slug}", function(Category $category){
         "currentCategory" => $category,
         "categories" => Category::all()
     ]);
-});
+})->name("category");
 
 Route::get("authors/{author:username}", function(User $author){
     // return view("posts", ["posts" => $author->posts->load(['category', 'author'])]);
