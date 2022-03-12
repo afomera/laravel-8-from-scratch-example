5e759a6ef2dec95db61d1bd49744ce1d4659bdb1
Andrea Fomera <afomera@hey.com>
2022-03-12 16:45:54 -0700

    kinda skip ahead to make more components but design the comment form

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,5 +1,4 @@
 <x-layout>
-
     <sction class="px-6 py-8">
         <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
             <article class="max-w-4xl mx-auto lg:grid lg:grid-cols-12 gap-x-10">
@@ -50,6 +49,8 @@
                 </div>
 
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
+                    @include ('posts._add-comment-form')
+
                     @foreach ($post->comments as $comment)
                          <x-post-comment :comment="$comment" />
                      @endforeach
