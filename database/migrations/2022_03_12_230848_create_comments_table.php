30604d0bef68f6cecc69e4b785ec64bc39c9de86
Andrea Fomera <afomera@hey.com>
2022-03-12 16:18:18 -0700

    migration to make comments table and keep things consistent

--- database/migrations/2022_03_12_230848_create_comments_table.php
+++ database/migrations/2022_03_12_230848_create_comments_table.php
@@ -13,16 +13,13 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('post_id')->constrained()->casacadeOnDelete();
             $table->foreignId('user_id')->constrained()->casacadeOnDelete();
-            $table->foreignId('category_id');
-            $table->string('title');
-            $table->string('slug')->unique();
-            $table->text('excerpt');
             $table->text('body');
-            $table->timestamp('published_at')->nullable();
             $table->timestamps();
+
         });
     }
 
@@ -33,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('comments');
     }
 };
