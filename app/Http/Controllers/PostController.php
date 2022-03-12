fd7c538f990cc012f6d4f151477bc44aca10ee89
Andrea Fomera <afomera@hey.com>
2022-03-12 12:49:10 -0700

    include the query string when there is stuff, so it doesn't loose search results

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,7 +11,7 @@ class PostController extends Controller
         return view("posts.index", [
             "posts" => Post::latest()->filter(
                     request(['search', 'category', 'author'])
-                )->paginate(5)
+                )->paginate(15)->withQueryString()
         ]);
     }
 
